b0fd5c057c507a2d2bed70ad6d4e61c725dea4c2
Denis <denis.spb.2004@gmail.com>
2026-02-23 17:43:02 +0300

    fix(iter13):

--- internal/db/postgres/migrations/20260223132456_change_type_id_in_shorten_url.sql
+++ internal/db/postgres/migrations/20260223132456_change_type_id_in_shorten_url.sql
@@ -5,16 +5,19 @@ ALTER TABLE shorten_url
 DROP SEQUENCE IF EXISTS shorten_url_id_seq;
 
 ALTER TABLE shorten_url 
-    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY 
-    (START WITH (SELECT MAX(id) + 1 FROM shorten_url));
+    ALTER COLUMN id TYPE BIGINT,
+    ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+
+SELECT setval(pg_get_serial_sequence('shorten_url', 'id'), COALESCE(MAX(id), 1)) FROM shorten_url;
 
 -- +goose Down
 ALTER TABLE shorten_url 
     ALTER COLUMN id DROP IDENTITY IF EXISTS;
 
 CREATE SEQUENCE shorten_url_id_seq
-    START WITH (SELECT MAX(id) + 1 FROM shorten_url)
     OWNED BY shorten_url.id;
 
+SELECT setval('shorten_url_id_seq', COALESCE(MAX(id), 1)) FROM shorten_url;
+
 ALTER TABLE shorten_url 
-    ALTER COLUMN id SET DEFAULT nextval('shorten_url_id_seq');
+    ALTER COLUMN id SET DEFAULT nextval('shorten_url_id_seq'::regclass);
